249bdb400129f21472e652e3246dd2a51be4aa1f
Erika Fröjd <erika.frojd1@learnet.se>
2020-09-23 12:42:54 +0200

    Add project files.

---
+++ BethanysPieShop/Models/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
+//ger oss möjligheten att arbeta med våra kategorier
---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; } //vi definerar att vi har en property
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+    }
+}
+
+//data för pies och categorys
